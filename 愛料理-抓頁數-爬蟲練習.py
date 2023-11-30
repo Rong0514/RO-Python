d2bab3c26c345cb8d9b96736bc07f7a870828a4c
吳威融 <148877689+Rong0514@users.noreply.github.com>
2023-11-30 15:52:31 +0800

    愛料理-抓頁數-爬蟲練習.py

--- 愛料理-抓頁數-爬蟲練習.py
+++ 愛料理-抓頁數-爬蟲練習.py
@@ -11,37 +11,38 @@ import requests
 
 import urllib.parse
 
-'''________________________________s  t  a  r  t________________________________'''  
+'''________________________________s  t  a  r  t____________________________'''  
 
 def delicious(seach1 , seach2):
 
-    session = requests.session()
+    session = requests.session()                                               # 以session方式做保持連接
     
-    url1    = 'https://icook.tw/'
+    url1    = 'https://icook.tw/'                                              # 請求網址
     
     header  = {'User-Agent':
               'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
-              }
+              }                                                                # 瀏覽器代理用戶(標頭)
         
-    '''________________________________傳送第一次請求__進入網頁________________________________'''  
+    '''________________________________傳送第一次請求__進入網頁_______________'''  
     
-    data    = session.get(url1 , headers = header)
+    data    = session.get(url1 , headers = header)                             # 將網址、標頭以session.get方式送回去
     
-    soup    = BeautifulSoup(data.text , 'html.parser')
+    soup    = BeautifulSoup(data.text , 'html.parser')                         # 帶入BeautifulSoup 用 html.parser解析 
     
-    '''________________________________傳送第二次請求__進入搜尋頁面________________________________'''  
+    '''________________________________傳送第二次請求__進入搜尋頁面___________'''  
     
-    url2    = 'https://icook.tw/recipes/search'
+    url2    = 'https://icook.tw/recipes/search'                                # 請求網址
     
-    payload = {'q': seach1,
+    payload = {'q': seach1,                                                    # payload要求  
                 'ingredients': seach2,
                 'page': ''
                 }
+    
     for i in range(2,100):
     
-        data    = session.get(url2 , headers = header , data = payload)
+        data    = session.get(url2 , headers = header , data = payload)        # 將網址、payload要求、標頭以 requests.get方式送回去
         
-        soup    = BeautifulSoup(data.text , "html.parser")
+        soup    = BeautifulSoup(data.text , "html.parser")                     # 帶入BeautifulSoup 用 html.parser解析 
         
         goods   = soup.select('li.browse-recipe-item')
         
@@ -60,34 +61,11 @@ def delicious(seach1 , seach2):
             
         payload['page'] = i
 
-'''________________________________e  n  d________________________________'''  
+'''________________________________e  n  d__________________________________'''  
     
 seach1  = input("輸入食譜名: ")
 seach2  = input("搜尋食材，以 , 分開: ")
 
 delicious(seach1, seach2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
