7173cf30f183d566a04723d39439f9d215d28687
吳威融 <148877689+Rong0514@users.noreply.github.com>
2023-11-30 15:53:11 +0800

    可樂旅遊-靜態-爬蟲練習.py

--- 可樂旅遊-靜態-爬蟲練習.py
+++ 可樂旅遊-靜態-爬蟲練習.py
@@ -9,40 +9,40 @@ from bs4 import BeautifulSoup
 
 import requests
 
-'''________________________________s  t  a  r  t________________________________'''  
+'''________________________________s  t  a  r  t____________________________'''  
 
 def Cola(seach):
     
-    session = requests.session()                                                # 以session方式做保持連接
+    session = requests.session()                                               # 以session方式做保持連接
     
-    url1    = 'https://www.colatour.com.tw/'                                    # 請求網址
+    url1    = 'https://www.colatour.com.tw/'                                   # 請求網址
     
     header  = {'User-Agent':
               'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
-              }                                                                 # 瀏覽器代理用戶(標頭)
+              }                                                                # 瀏覽器代理用戶(標頭)
     
-    '''________________________________傳送第一次請求__進入網頁________________________________'''  
+    '''________________________________傳送第一次請求__進入網頁_______________'''  
  
-    data    = session.get(url1 , headers = header)                              # 將網址、標頭以session.get方式送回去
+    data    = session.get(url1 , headers = header)                             # 將網址、標頭以session.get方式送回去
     
-    p1 = 'DepartureCity=*'                                                      # 補足網址要求並給予彈性
+    p1 = 'DepartureCity=*'                                                     # 補足網址要求並給予彈性
     p2 = 'KeyWord={}'.format(seach)
     p3 = 'Source=00'
     
     url2    = 'https://www.colatour.com.tw/C10A_TourSell/C10A08_PatternSearch.aspx?{}&{}&{}'.format(p1,p2,p3)
-                                                                                # 請求網址
+                                                                               # 請求網址
                                                                                 
-    payload = {                                                                 # 網頁要求  
+    payload = {                                                                # payload要求  
                 'DepartureCity': '*',
                 'KeyWord': seach,
                 'Source': '00'
                   }
     
-    '''________________________________傳送第二次請求__進入搜尋頁面________________________________'''  
+    '''________________________________傳送第二次請求__進入搜尋頁面___________'''  
     
-    data    = session.get(url2 , headers = header , params = payload)           # 將網址、要求參數、標頭以requests.get方式送回去
+    data    = session.get(url2 , headers = header , params = payload)          # 將網址、payload要求、標頭以 requests.get方式送回去
     
-    soup    = BeautifulSoup(data.text , 'html.parser')
+    soup    = BeautifulSoup(data.text , 'html.parser')                         # 帶入BeautifulSoup 用 html.parser解析 
     
     total   = soup.select('ul.tourin-city-list-block li')
     
@@ -63,8 +63,7 @@ def Cola(seach):
             print("_"*50)
             print()
         
-        
-'''________________________________e  n  d________________________________'''  
+'''________________________________e  n  d__________________________________'''  
 
     
 seach = input("請輸入關鍵字: ")
